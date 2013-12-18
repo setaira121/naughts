17e66fb4b785e47e93cea69407813697ea61ad59
Hamish Kendall <hamishkendall@yahoo.com>
2013-12-18 23:01:26 +1300

    added canvas

--- js/application.js
+++ js/application.js
@@ -1,1 +1,9 @@
-alert("Hello world");
+var canvas = document.getElementById("game_canvas");
+var context = canvas.getContext("2d");
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
+context.fillStyle = "rgba(255,0,0,1)"
+context.fillRect(0,0,canvasWidth,canvasHeight);
+
+context.fillStyle = "rgba(0,255,0,0.5)"
+context.fillRect(0,0,canvasWidth/2,canvasHeight/2);
